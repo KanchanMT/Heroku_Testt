811e2ec68a4c7f3e7c7f2bdfd057acc8fa33ebc9
KanchanMT <31151512+KanchanMT@users.noreply.github.com>
2021-09-13 21:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='The GDP of the Country should be'.format(final_features))
+    return render_template('index.html', prediction_text='The GDP of the Country should be $ {}'.format(final_features))
 
 if __name__ == "__main__":
     app.run(debug=True)
